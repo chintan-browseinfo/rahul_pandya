5226795de02db8b5c910b8ba7c8678fea2961a0a
Pandya Rahul <Rahul.pandya@browseinfo.in>
2018-06-25 16:34:42 +0530

    [ADD]added new student module.

---
+++ student/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Job Portal Management',
+    'summary':'Jobportal',
+    'author':'Pandya Rahul',
+    'company':'BrowseInfo',
+    'category':'Job Portal',
+    'Website':'pandyarahul.esy.es',
+    'data' : ['views/views.xml'],
+    'installable':'True',
+    'application':'False'
+}
---
+++ student/models/student.py
@@ -0,0 +1,29 @@
+from odoo import api, fields, models, _
+
+
+class studentstudent(models.Model):
+    _name = "student.student"
+    _description = "Jobportal"
+
+    name = fields.Char(string="Name", required=True)
+    lastname = fields.Char(string="Last Name", required=True)
+    address = fields.Char(string="Address", required=True)
+    city = fields.Char(string="City", required=True)
+    dob = fields.Date(string="D.O.B", required=True)
+    age = fields.Integer(string="Age", required=True)
+    gender = fields.Selection([('m','Male'),('f','Femele')],string="Gender", required=True)  
+    Location = fields.Many2one('res.partner', String="Location")
+    
+class studentjob(models.Model):
+    _name = "student.job"
+    _description = "Jobportal"
+
+    degree = fields.Selection([('g','Graduate'),('pg','Post Graduate'),('dp','Diploma')],string="Qualification", required=True)
+    year = fields.Char(string="Passing Year", required=True)
+    per = fields.Integer(string="Persentage", required=True)
+    application = fields.Selection([('d','Devloper'),('pm','Project Manager'),('tl','Team Leader'),                                     
+                                        ('tr','Trainee')],string="Apply For",required=True)
+    language = fields.Selection([('o','Odoo'),('p','Python'),('x','XML'),('j','Java')],string="Interested In",required=True)
+    company_id = fields.Many2one('student.student', String="company")
+    
+    
